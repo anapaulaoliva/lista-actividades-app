b7e9ec457ee9979aece38af2787d8459f3f9575d
AP <ap@MacBook-Pro-de-lilu.local>
2025-03-18 17:51:00 -0600

    Agregar selectores

--- cypress/e2e/primera_prueba.cy.ts
+++ cypress/e2e/primera_prueba.cy.ts
@@ -1,6 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('/')
-    cy.get('h1').should('contain', 'Lista de Actividades')
+    cy.wait(10_000)
+    cy.get('h1', {timeout: 1000}).should('contain', 'Lista de Actividades');
+    cy.contains('Agregar').click()
   })
 })
